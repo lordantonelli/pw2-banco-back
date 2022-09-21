2b295bb1e18e60beaf3b9273d1db2349471e632a
Humberto Lidio Antonelli <humbertoantonelli@usp.br>
2022-09-21 01:06:53 -0400

    feat: implementar entidade de agência bancária

--- src/bank-branchs/bank-branchs.module.ts
+++ src/bank-branchs/bank-branchs.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { BankBranchsService } from './bank-branchs.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { BankBranchsController } from './bank-branchs.controller';
+import { BankBranchsService } from './bank-branchs.service';
+import { BankBranch } from './entities/bank-branch.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BankBranch])],
   controllers: [BankBranchsController],
-  providers: [BankBranchsService]
+  providers: [BankBranchsService],
 })
 export class BankBranchsModule {}
--- src/bank-branchs/entities/bank-branch.entity.ts
+++ src/bank-branchs/entities/bank-branch.entity.ts
@@ -1,1 +1,21 @@
-export class BankBranch {}
+import { BaseEntity, Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+export class BankBranch extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 255 })
+  name: string;
+
+  @Column({ type: 'int', unique: true })
+  code: number;
+
+  @Column()
+  isActive: boolean;
+
+  @CreateDateColumn()
+  dateCreated: Date;
+
+  @UpdateDateColumn()
+  lastUpdated: Date;
+}
