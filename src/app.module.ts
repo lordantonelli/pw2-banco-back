d4e0540f0e5d3194dcac51667f3255206080fab2
Humberto Lidio Antonelli <humbertoantonelli@usp.br>
2022-09-21 00:43:46 -0400

    feat: adicionar configuração de conexão com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BankBranchsModule } from './bank-branchs/bank-branchs.module';
 
 @Module({
-  imports: [BankBranchsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database/banco',
+      entities: [__dirname + '/**/*.entity.{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
