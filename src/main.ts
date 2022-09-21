32ec2e7bebc24aaaed77feeb054569951130aa02
Humberto Lidio Antonelli <humbertoantonelli@usp.br>
2022-09-21 01:34:07 -0400

    feat: adicionar validação em escopo global

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ValidationError } from 'class-validator';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        return new BadRequestException(validationErrors);
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
